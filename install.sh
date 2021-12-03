77686323ac784955c882ba385acb2dc954694165
NickolasHKraus <NickHKraus@gmail.com>
2021-12-03 14:06:20 -0600

    Update install.sh script

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+#
+# DESCRIPTION
+# Link Bash scripts to $HOME/bin.
+#
+# USAGE
+#
+#   install.sh
+#
+# ADDITIONAL INFORMATION
+#
+# Add the following to your shell configuration file:
+#
+#   ```bash
+#   export PATH=$HOME/bin:$PATH
+#   ```
+#
+# Link executables to `$HOME/bin`:
+#
+#   ```bash
+#   ./install.sh
+#   ```
 
-mkdir -p $HOME/bin
+mkdir -p "${HOME}"/bin
 
-dir="$(pwd)"
-
-if [[ $dir != */ ]]; then
-  dir="$dir/*"
-else
-  dir="$dir*"
-fi
-
-for f in $dir; do
-  [ ! -d "${f}" ] && [ -x "${f}" ] && [ $(basename "$f") != "install.sh" ] || continue
-  ln -s $f $HOME/bin/ 2> /dev/null
+for f in "$(pwd)"/*; do
+  [ ! -d "${f}" ] && [ -x "${f}" ] && [ "$(basename "${f}")" != "install.sh" ] || continue
+  ln -s "${f}" "${HOME}"/bin 2>/dev/null
 done
